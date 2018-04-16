b290c8459e7e70aaddc8a176257dd3998bb4418d
Darren Carlin <darrencarlin@Darrens-MacBook-Pro.local>
2018-04-17 00:07:56 +0100

    search functionality

--- script.js
+++ script.js
@@ -25,14 +25,22 @@ let endpoint = "https://www.darrencarlin.com/games.json";
 // Flatten / De-duplicate Arrays.
 
 let flattenSort = (arr) => {
-    return [...new Set(arr.reduce((a, b) => a.concat(b), []))]
+    return [...new Set(arr.reduce((a, b) => a.concat(b), []))];
 }
+
 
 // Find max player Count off all games.
 
 let findMax = (arr) => {
-    return [Math.max(...arr)]
+    return [Math.max(...arr)];
 }
+
+// Remove empty strings from Array 
+
+let removeEmpty = (arr) => {
+    arr.filter(str => str.trim() != '');
+};
+
 
 // Create and populate options for select menus.
 // *numberedList is used for player 2 - 'max player' 
@@ -45,51 +53,81 @@ let populateOption = (jsArr, htmlEl, numberedList = false) => {
         }
     } else {
         for (let i of jsArr) {
-            if (i === "") { // Incase JSON contains an empty string 
-                continue // Don't include it
-            }
             htmlEl.innerHTML += `<option value="${i}"> ${i} </option>`;
         }
     }
 }
 
-// AJAX Call 
+const gamez = [];
 
 fetch(endpoint)
-    .then(response => response.json())
-    .then(data => {
+    .then(blob => blob.json())
+    .then(data => gamez.push(...data));
 
-        data.forEach((game, index) => { 
+gamez.forEach((game) => {
+    games.push(game.name);
+})
 
-            gameArr.push(game); // Storing entire JSON data.
-
-            games.push(game.name); // Storing Names
-
-            genres.push(game.genre); // Storing Genres
-            genres = flattenSort(genres);
-
-            ideal.push(game.idealFor); // Storing Ideal For
-            ideal = flattenSort(ideal);
-
-            players.push(game.players.playersMax); // Storing Max Players
-            players = findMax(players);
-
-            mechanics.push(game.mechanics); // Storing Mechanics
-            mechanics = flattenSort(mechanics);
-
-            difficulty.push(game.difficulty); // Storing Difficulty 
-            difficulty = flattenSort(difficulty);
-
-        });
-
-        populateOption(games, gamesList);
-        populateOption(genres, genresList);
-        populateOption(ideal, idealList);
-        populateOption(mechanics, mechanicsList);
-        populateOption(difficulty, difficultyList);
-        populateOption(players, playerList, true);
-
+function findMatches(wordToMatch, gamez) {
+    return gamez.filter(item => {
+        const regex = new RegExp(wordToMatch, 'gi');
+        return item.name.match(regex)
     })
-    .catch(function (error) {
-        console.log(error);
-    });
+}
+
+function displayMatches() {
+    const matchArray = findMatches(this.value, gamez)
+    const html = matchArray.map(item => {
+        return `
+        <li>
+            ${item.name}
+        </li>
+        `;
+    }).join('');
+    suggestions.innerHTML = html;
+}
+
+const searchInput = document.querySelector('.search-bar');
+const suggestions = document.querySelector('.suggestions');
+
+searchInput.addEventListener('change', displayMatches);
+searchInput.addEventListener('keyup', displayMatches);
+
+// // AJAX Call 
+// let gamez = [];
+// fetch(endpoint)
+//     .then(response => response.json())
+//     //  .then(jsonData => gamez.push(...jsonData))
+//     .then(data => data.forEach((game, index) => {
+
+//         gameArr.push(game); // Storing entire JSON data.
+
+//         games.push(game.name); // Storing Names
+
+//         genres.push(game.genre); // Storing Genres
+//         genres = (flattenSort(genres));
+
+//         ideal.push(game.idealFor); // Storing Ideal For
+//         ideal = flattenSort(ideal);
+
+//         players.push(game.players.playersMax); // Storing Max Players
+//         players = findMax(players);
+
+//         mechanics.push(game.mechanics); // Storing Mechanics
+//         mechanics = flattenSort(mechanics);
+
+//         difficulty.push(game.difficulty); // Storing Difficulty 
+//         difficulty = flattenSort(difficulty);
+
+//         populateOption(games, gamesList);
+//         populateOption(genres, genresList);
+//         populateOption(ideal, idealList);
+//         populateOption(mechanics, mechanicsList);
+//         populateOption(difficulty, difficultyList);
+//         populateOption(players, playerList, true);
+
+
+//     }))
+//     .catch(function (error) {
+//         console.log(error);
+//     });
